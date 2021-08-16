0091ea8ff466cb605762c909759d93d12f92409a
unknown <60645387+BamgoeSN@users.noreply.github.com>
2021-08-16 10:28:43 +0900

    Baby shark inputgen

---
+++ inputgen/16236 (baby shark)/inputgen.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"strings"
+)
+
+var (
+	// Board size range
+	nmin, nmax int = 2, 6
+)
+
+func CreateInput() string {
+	ans := new(strings.Builder)
+
+	N := rand.Intn(nmax-nmin+1) + nmin
+	fmt.Fprintln(ans, N)
+	arr := make([][]int, N)
+	for r := range arr {
+		arr[r] = make([]int, N)
+	}
+
+	rate := make([]float64, 6)
+	for i := range rate {
+		rate[i] = rand.Float64()
+	}
+	sort.Float64s(rate)
+
+	for r := range arr {
+		for c := range arr[r] {
+			pick := rand.Float64()
+			arr[r][c] = sort.Search(len(rate), func(i int) bool { return pick < rate[i] })
+		}
+	}
+
+	sr, sc := rand.Intn(N), rand.Intn(N)
+	arr[sr][sc] = 9
+
+	for _, r := range arr {
+		for _, e := range r {
+			fmt.Fprintf(ans, "%d ", e)
+		}
+		fmt.Fprintln(ans)
+	}
+
+	return TrimSpaces(ans.String())
+}
+
+func TrimSpaces(str string) string {
+	flds := strings.Split(str, "\n")
+	for i, v := range flds {
+		flds[i] = strings.TrimSpace(v)
+	}
+	return strings.TrimSpace(strings.Join(flds, "\n"))
+}
